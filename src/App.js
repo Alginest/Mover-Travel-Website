7aa09f2fb7fba56f4076f689842290915beffcda
ado durak <76266634+Alginest@users.noreply.github.com>
2021-07-30 14:27:10 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
+import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import Products from './components/pages/Products'
+import SignUp from './components/pages/SingUp'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact>
+            {Home}
+          </Route>
+          <Route path='/services'>{Services}</Route>
+          <Route path='/products'>{Products}</Route>
+          <Route path='/sign-up'>{SignUp}</Route>
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+export default App
